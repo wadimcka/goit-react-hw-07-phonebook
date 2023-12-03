16a2697fd17e955f293653e5b28c71b8b38cf7af
Vadym Bobyrenko <wadimcka@gmail.com>
2023-12-03 15:12:24 +0100

    add createSelector

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,25 +1,21 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { selectContacts, selectFilter, selectisLoading } from 'redux/selectors';
+import { selectfiltredContacts, selectisLoading } from 'redux/selectors';
 import { fetchAllContactsThunk } from 'redux/operations';
 import { ListOfContacts } from './ContactList.styled';
 import ContactItem from 'components/Contact/ContactItem';
 import { Loader } from 'components/Loader/Loader';
 
 function ContactList() {
-  const filter = useSelector(selectFilter);
   const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts);
+
   const isLoading = useSelector(selectisLoading);
+  const filteredContacts = useSelector(selectfiltredContacts);
 
   useEffect(() => {
     dispatch(fetchAllContactsThunk());
   }, [dispatch]);
-
-  const filteredContacts = contacts?.filter(contact =>
-    contact?.name?.toLowerCase().includes(filter.toLowerCase())
-  );
 
   return (
     <ListOfContacts>
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,8 +1,15 @@
-// import { createSelector } from '@reduxjs/toolkit';
+import { createSelector } from '@reduxjs/toolkit';
 
 export const selectContacts = state => state.contacts.items;
 
 export const selectFilter = state => state.filter;
 export const selectisLoading = state => state.contacts.isLoading;
 
-// export const selectFiltredContacts = createSelector([selectFilter]);
+export const selectfiltredContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+);
